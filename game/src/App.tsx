edc1e086ff3e2bb01e5f71805bb14e0d49841c61
vaibhav waghmare <itsmevaibhavwaghmare7666@gmail.com>
2023-04-09 14:26:13 +0530

    adding gametsx

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { game } from './Game/game';
 
 function App() {
   return (
     <div className="App">
-    <h1 className='text-red-500 border-black' > hi  this is Game devs team ..</h1>
+     
     </div>
   );
 }
